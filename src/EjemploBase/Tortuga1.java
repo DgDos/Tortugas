2e76e65a1229c615c99520035dd9fc2e0036c266
white <white@WHITE1008.academia.loc>
2019-02-19 13:15:46 -0500

    Tortuga1

---
+++ src/EjemploBase/Tortuga1.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EjemploBase;
+
+import ch.aplu.turtle.Turtle;
+
+/**
+ *
+ * @author white
+ */
+public class Tortuga1 extends Thread {
+
+    private Turtle tortuga;
+
+    public Tortuga1(Turtle tortuga) {
+        this.tortuga = tortuga;
+    }
+
+    @Override
+    public void run() {
+        this.tortuga.setPos(-200,0);
+        this.tortuga.right(90);
+        this.tortuga.forward(185);
+        this.tortuga.penUp();
+        this.tortuga.setPos(-10,0);
+        this.tortuga.penDown();
+        this.tortuga.left(90);
+        this.tortuga.forward(5);
+        this.tortuga.right(90);
+        this.tortuga.forward(10);
+        this.tortuga.right(90);
+        this.tortuga.forward(10);
+        this.tortuga.right(90);
+        this.tortuga.forward(10);
+        this.tortuga.right(90);
+        this.tortuga.forward(5);
+        this.tortuga.penUp();
+        this.tortuga.setPos(5,0);
+        this.tortuga.penDown();
+        this.tortuga.forward(5);
+        this.tortuga.right(90);
+        this.tortuga.forward(10);
+        this.tortuga.right(90);
+        this.tortuga.forward(10);
+        this.tortuga.right(90);
+        this.tortuga.forward(10);
+        this.tortuga.right(90);
+        this.tortuga.forward(5);
+        
+        
+        this.tortuga.setPos(20,0);
+        this.tortuga.forward(5);
+        this.tortuga.right(90);
+        this.tortuga.forward(10);
+        this.tortuga.right(90);
+        this.tortuga.forward(10);
+        this.tortuga.right(90);
+        this.tortuga.forward(10);
+        this.tortuga.right(90);
+        this.tortuga.forward(5);
+        
+        this.tortuga.right(90);
+        this.tortuga.penUp();
+        this.tortuga.setPos(85,0);
+        this.tortuga.penDown();
+        this.tortuga.forward(115);
+        
+        
+    }
+
+}
